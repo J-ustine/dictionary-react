f2a77aa3a7cd1851904edb8dab557c34bfe5ac10
J-ustine <78701219+J-ustine@users.noreply.github.com>
2021-03-24 20:00:34 +0100

    Added Api call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(null);
   function handlekeyword(event) {
     setKeyword(event.target.value);
   }
+  function displayDefinition(response) {
+    console.log(response.data);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(displayDefinition);
+  }
   return (
-    <form>
+    <form onSubmit={search}>
       <input
         type="search"
         placeholder="Enter a word"
         onChange={handlekeyword}
       />
       <input type="submit" value="Search" />
-      {keyword}
     </form>
   );
 }
